24773aa91f23d3cf671f697318825d66802968fb
jay <jaeykweon@naver.com>
2023-08-31 02:08:17 +0900

    project init

---
+++ src/main/kotlin/org/idd/kopringlab/KopringLabApplication.kt
@@ -0,0 +1,11 @@
+package org.idd.kopringlab
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KopringLabApplication
+
+fun main(args: Array<String>) {
+    runApplication<KopringLabApplication>(*args)
+}
---
+++ src/test/kotlin/org/idd/kopringlab/KopringLabApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.idd.kopringlab
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KopringLabApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
